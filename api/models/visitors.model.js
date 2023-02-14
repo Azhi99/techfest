0f08b6c5a7f5a6aab7de77acc6f5c3b2a8249f35
Azhi99 <azhijabar.aj@gmail.com>
2023-02-14 20:54:40 +0300

    fix datetime registration

--- api/models/visitors.model.js
+++ api/models/visitors.model.js
@@ -37,7 +37,7 @@ class Visitors {
     }
     async createOne(data) {
         var d = new Date();
-        d.setHours(d.getHours() - 3);
+        d.setHours(d.getHours() + 3);
         data.visitor_registration_datetime = d
         const insertedData = await db(table_name).insert(data)
             .then(data => {
