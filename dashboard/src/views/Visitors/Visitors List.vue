623b06f799485fd1291aa9c56b358305a672590f
Azhi99 <azhijabar.aj@gmail.com>
2023-02-17 16:45:42 +0300

    edit production

--- dashboard/src/views/Visitors/Visitors List.vue
+++ dashboard/src/views/Visitors/Visitors List.vue
@@ -5,7 +5,7 @@
                 <v-card>
                     <v-card-text>
                         <v-text-field label="Search" v-model="search"></v-text-field>
-                        <v-data-table :headers="headers" show-select v-model="selected_rows"  :search="search" :items="rows" class="elevation-0"
+                        <v-data-table v-if="$route.query.key && $route.query.key == 'tech_rzh23'" :headers="headers" show-select v-model="selected_rows"  :search="search" :items="rows" class="elevation-0"
                             item-key="visitor_id">
                             <template v-slot:[`item.visitor_id`]="{ item }">
                                 <div>
@@ -24,10 +24,29 @@
                                 </export-excel>
                             </template>
                         </v-data-table>
+
+
+
+                        <v-data-table v-else :headers="headers" hide-default-footer show-select v-model="selected_rows"  :search="search" :items="rows" class="elevation-0"
+                            item-key="visitor_id">
+                            <template v-slot:[`item.visitor_id`]="{ item }">
+                                <div>
+                                    <!-- <v-btn icon :to="'/visitors-list/'+item.visitor_id"  color="teal" class="mx-1" v-if="$route.query.key && $route.query.key == 'tech_rzh23'">
+                                        <v-icon> mdi-pencil-outline </v-icon>
+                                    </v-btn>
+                                    <v-btn color="error" icon class="mx-1" @click="selectVisitors(item)" >
+                                        <v-icon> mdi-delete-outline </v-icon>
+                                    </v-btn> -->
+                                </div>
+                            </template>
+                            
+                        </v-data-table>
                     </v-card-text>
                     
-                    <v-card-actions v-if="selected_rows.length > 0">
-                        <v-btn color="error" @click="deleteVisitorsList">{{$store.getters.language.data.visitors.delete_list_btn}}</v-btn>
+                    <v-card-actions class="justify-end">
+                        <div class="text-end">
+                            <small> Visitor: {{ rows.length + (rows.length / 5) }} </small>
+                        </div>
                     </v-card-actions>
                         
                 </v-card>
